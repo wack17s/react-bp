177e06cdce686f224b7a08872ab42ee3db721a52
wack17s <keroone17@gmail.com>
2017-06-03 18:07:58 +0300

    add hmr

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React        from 'react';
-import ReactDOM     from 'react-dom';
-import { Provider } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
 import { polyfill } from 'smoothscroll-polyfill';
 
-import App from './App';
-
 import registerServiceWorker from './utils/registerServiceWorker';
-import configureStore        from './store/configureStore';
+import configureStore from "./store/configureStore";
 
 import './index.css';
 
@@ -14,11 +12,52 @@ polyfill();
 
 const store = configureStore();
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+// Save a reference to the root element for reuse
+const rootEl = document.getElementById("root");
+
+// Create a reusable render method that we can call more than once
+let render = () => {
+    // Dynamically import our main App component, and render it
+    const App = require('./App').default;
+    
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        rootEl
+    );
+};
+
+if (module.hot) {
+    // Support hot reloading of components
+    // and display an overlay for runtime errors
+    const renderApp = render;
+    const renderError = (error) => {
+        const RedBox = require('redbox-react').default;
+        ReactDOM.render(
+            <RedBox error={error} />,
+            rootEl,
+        );
+    };
+
+    // In development, we wrap the rendering function to catch errors,
+    // and if something breaks, log the error and render it to the screen
+    render = () => {
+        try {
+            renderApp();
+        } catch (error) {
+            console.error(error);
+            renderError(error);
+        }
+    };
+
+    // Whenever the App component file or one of its dependencies
+    // is changed, re-import the updated component and re-render it
+    module.hot.accept('./App', () => {
+        setTimeout(render);
+    });
+}
+
+render();
 
 registerServiceWorker();
