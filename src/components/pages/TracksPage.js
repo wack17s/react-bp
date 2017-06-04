71912c413d81b14618f85b573f77bbd9fe315770
unsigned6 <yuriivlasiuk@gmail.com>
2017-06-05 00:11:01 +0300

    Removed unnecessary imports.

--- src/components/pages/TracksPage.js
+++ src/components/pages/TracksPage.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes            from 'prop-types';
-import { connect }          from 'react-redux';
 import cx                   from 'classnames';
-import * as TracksActions   from '../../actions/TracksActions';
 
 import logo from '../../logo.svg';
 
