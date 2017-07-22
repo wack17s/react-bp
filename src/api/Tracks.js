729ffeeec3289589577b5bd3ebb6b5805734eb02
wack17s <keroone17@gmail.com>
2017-07-22 15:59:55 +0300

    redux version

--- src/api/Tracks.js
+++ src/api/Tracks.js
@@ -6,19 +6,19 @@ class Tracks extends Base {
         return [
             {
                 id     : 1,
-                url    : 'https://p4.bcbits.com/download/track/693fbdaadf6c49b76e09bdef2139753a/mp3-128/15821142?fsig=176920eab07717d5d5f2061d092075d8&id=15821142&stream=1&ts=1496520000.0&token=1496520060_3544ffe1c59755ba8ca3109a3a537af33caa0d4f',
-                title  : 'Coolverine',
-                artist : 'Mogwai',
-                album  : 'Every Country\'s Sun',
-                year   : 2017
+                url    : 'http://cdn.anything2mp3.com/system/files/mp3/The%20Black%20Keys-Busted_FVnBU3tIci8_youtube.mp3',
+                title  : 'Busted',
+                artist : 'Black Keys',
+                album  : 'The Big Come Up',
+                year   : 2002
             },
             {
                 id     : 2,
-                url    : 'https://p4.bcbits.com/download/track/efb92ed8d233e75a42ee525759d2a8d5/mp3-128/759874955?fsig=0606e82472da246476ed0c1def133be3&id=759874955&stream=1&ts=1496505600.0&token=1496505660_87b7598501939b02f9e95cb5e5516c8efa82492c',
-                title  : 'Parks On Fire',
-                artist : 'Trifonic',
-                album  : 'Emergence',
-                year   : 2008
+                url    : 'http://cdn.anything2mp3.com/system/files/mp3/THE%20BLACK%20KEYS%20-%20I%27ll%20be%20your%20man%20_%20LYRICS_wx1IOZNHHUc_youtube.mp3',
+                title  : 'I\'ll be your man',
+                artist : 'Black Keys',
+                album  : 'The Big Come Up',
+                year   : 2002
             }
         ];
     }
